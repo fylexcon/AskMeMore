993e551da280b7f1a76e0963d372b8c83ee22181
fylexcon <s.suna604@gmail.com>
2026-07-08 14:11:35 +0300

    fix: use global.ErrorUtils to fix metro bundler resolution error

--- apps/mobile/index.js
+++ apps/mobile/index.js
@@ -1,16 +1,17 @@
 import { Alert } from "react";
-import ErrorUtils from "ErrorUtils";
 
-const defaultHandler = ErrorUtils.getGlobalHandler();
-ErrorUtils.setGlobalHandler((error, isFatal) => {
-  Alert.alert(
-    "Fatal JS Error",
-    `Error: ${error.message}\n\nStack: ${error.stack}`,
-    [{ text: "OK" }]
-  );
-  if (defaultHandler) {
-    defaultHandler(error, isFatal);
-  }
-});
+const defaultHandler = global.ErrorUtils?.getGlobalHandler?.();
+if (global.ErrorUtils) {
+  global.ErrorUtils.setGlobalHandler((error, isFatal) => {
+    Alert.alert(
+      "Fatal JS Error",
+      `Error: ${error.message}\n\nStack: ${error.stack}`,
+      [{ text: "OK" }]
+    );
+    if (defaultHandler) {
+      defaultHandler(error, isFatal);
+    }
+  });
+}
 
 import "expo-router/entry";
