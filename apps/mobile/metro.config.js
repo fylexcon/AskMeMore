1cf80405cb14aedda165d409d56f49f599902976
fylexcon <s.suna604@gmail.com>
2026-07-08 14:06:07 +0300

    fix: append to watchFolders instead of overriding to pass expo-doctor during eas build

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -3,7 +3,7 @@ const { getDefaultConfig } = require("expo/metro-config");
 
 const config = getDefaultConfig(__dirname);
 
-config.watchFolders = [path.resolve(__dirname, "../../packages")];
+config.watchFolders = [...(config.watchFolders ?? []), path.resolve(__dirname, "../../packages")];
 config.resolver.nodeModulesPaths = [
   path.resolve(__dirname, "node_modules"),
   path.resolve(__dirname, "../../node_modules"),
