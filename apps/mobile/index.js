c66c95715e63b0d9f891a11d5d66dd9dcde76679
fylexcon <s.suna604@gmail.com>
2026-07-08 14:00:21 +0300

    fix: re-enable new architecture and add global JS error handler to debug crash

--- apps/mobile/index.js
+++ apps/mobile/index.js
@@ -1,1 +1,16 @@
+import { Alert } from "react";
+import ErrorUtils from "ErrorUtils";
+
+const defaultHandler = ErrorUtils.getGlobalHandler();
+ErrorUtils.setGlobalHandler((error, isFatal) => {
+  Alert.alert(
+    "Fatal JS Error",
+    `Error: ${error.message}\n\nStack: ${error.stack}`,
+    [{ text: "OK" }]
+  );
+  if (defaultHandler) {
+    defaultHandler(error, isFatal);
+  }
+});
+
 import "expo-router/entry";
